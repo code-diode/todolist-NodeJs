4ca3c60a9cabe5560ffc2e6fcc33746b681ce521
Code-Diode <78120836+Code-Diode@users.noreply.github.com>
2022-06-21 12:48:48 +0300

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -172,8 +172,11 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
+let port = process.env.PORT;
+if(port == null || port == "") {
+  port = 3000;
+}
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
